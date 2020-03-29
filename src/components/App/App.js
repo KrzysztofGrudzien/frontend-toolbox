4016b4e71c39900e75cc26d97bd7a30abd1e0540
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2020-03-30 00:43:04 +0200

    create a basic structure of app

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../../assets/images/logo.svg';
+import '../App/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome in the FrontEnd ToolBOX App</p>
+        <p>You have to be patient 😎 - we will be work very hard to build useful frontend tools.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
