05c3a8778070363fb59cf2313abc2a93537af344
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2020-04-01 15:57:17 +0200

    feat: Install, configure and teste storybook

---
+++ src/components/Input/Input.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+        color: white;
+        padding: 15px 30px;
+        font-size: 12px;
+        font-weight: bold;
+        text-align: center;
+        background-color: lightgray;
+        border: none;
+        border-radius: 50px;
+`;
+
+export default Input;
---
+++ src/components/Input/Input.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Input from './Input';
+
+export default { title: 'Input' };
+
+export const clearInput = () => <Input placeholder="Click me" />;
