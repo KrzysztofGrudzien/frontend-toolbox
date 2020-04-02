dfa5e09249835cf4e42d805d6b91956e8c40789f
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2020-04-02 09:23:48 +0200

    chore: Create globalStyle and basic mainTheme

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
+
+    *, *::before, *::after {
+        box-sizing: border-box;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+    html {
+        font-size: 62.5%;
+    }
+
+    body {
+        padding-left: 180px;
+        font-size: 1.6rem;
+        font-family: 'Poppins', sans-serif;
+    }
+`;
+
+export default GlobalStyle;
---
+++ src/theme/mainTheme.js
@@ -0,0 +1,26 @@
+export const theme = {
+    colors: {
+        primary: '#222831',
+        secondary: '#393E46',
+        tertiary: '#5D6167',
+        additional: '#D65A31',
+    },
+    fontColors: {
+        grayPrimary: '#EEEEEE',
+        gray60: '#A6A8AB',
+        gray80: '#CACBCC',
+    },
+    fontSize: {
+        xxs: '1rem',
+        xs: '1.2rem',
+        s: '1.4rem',
+        m: '1.6rem',
+        l: '2rem',
+        xl: '4.8rem',
+    },
+    weigth: {
+        regular: 400,
+        medium: 500,
+        bold: 600,
+    },
+};
