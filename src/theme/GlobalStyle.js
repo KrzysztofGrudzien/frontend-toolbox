cfd05861fdf11ba07ccf2d2949a4c0e0670f9018
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2020-04-03 18:57:02 +0200

    chore: Intal normalize to global styles

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
 
 const GlobalStyle = createGlobalStyle`
+    ${normalize}
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
 
     *, *::before, *::after {
