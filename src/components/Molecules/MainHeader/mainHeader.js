805033b6f19cfb8603e05ca693c953fe76bd200e
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2020-04-22 16:47:03 +0200

    fix: move a structure component from stories to correct file

--- src/components/Molecules/MainHeader/mainHeader.js
+++ src/components/Molecules/MainHeader/mainHeader.js
@@ -1,6 +1,10 @@
+import React from 'react';
 import styled from 'styled-components';
+import Paragraph from '../../Atoms/Paragraph/Paragraph';
+import Search from '../../Atoms/Input/Input';
+import Icon from '../../Atoms/Icon/Icon';
 
-const Header = styled.header`
+const HeaderOne = styled.header`
     background-color: ${({ theme }) => theme.colors.primary};
     display: grid;
     grid-template-columns: 1fr 2fr 70px;
@@ -9,5 +13,20 @@ const Header = styled.header`
     width: calc(100% - 255px);
     margin-bottom: 3.2rem;
 `;
+
+const today = new Date();
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+const formattedDate = `
+        ${day[today.getDay()]}   ${today.getDate()}   ${
+    months[today.getMonth()]
+},  ${today.getFullYear()}`;
+const Header = () => (
+    <HeaderOne>
+        <Paragraph secondary>{formattedDate}</Paragraph>
+        <Search search placeholder="Search" />
+        <Icon user />
+    </HeaderOne>
+);
 
 export default Header;
--- src/components/Molecules/MainHeader/mainHeader.stories.js
+++ src/components/Molecules/MainHeader/mainHeader.stories.js
@@ -1,21 +1,6 @@
 import React from 'react';
 import Header from './mainHeader';
-import Paragraph from '../../Atoms/Paragraph/Paragraph';
-import Search from '../../Atoms/Input/Input';
-import Icon from '../../Atoms/Icon/Icon';
 
 export default { title: 'Molecules/mainHeader' };
-const today = new Date();
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-const day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-const formattedDate = `
-        ${day[today.getDay()]}   ${today.getDate()}   ${
-    months[today.getMonth()]
-},  ${today.getFullYear()}`;
-export const MainHeader = () => (
-    <Header>
-        <Paragraph secondary>{formattedDate}</Paragraph>
-        <Search search placeholder="Search" />
-        <Icon user />
-    </Header>
-);
+
+export const MainHeader = () => <Header />;
